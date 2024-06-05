ef3c1722ac0e4b7749775b5ea3b7e13beb7bbcd0
benny <vvsogi@gmail.com>
2024-06-05 17:42:27 +0900

    Add task1 choice page and update task1 page

---
+++ app/task1/choice/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { MoveBackHeader, MoveButton } from "@/app/components";
+
+export default function page() {
+  return (
+    <div>
+      <MoveBackHeader />
+      <div className="flex flex-col gap-[50px] mt-[305px] mb-[175px]">
+        <MoveButton path="/task1/choice/top">상의</MoveButton>
+        <MoveButton path="/task1/choice/bottom">하의</MoveButton>
+        <MoveButton path="/task1/choice/outer">아우터</MoveButton>
+      </div>
+      <div className="flex mx-auto">
+        <MoveButton path="/task1/choice/result">코디맵</MoveButton>
+      </div>
+    </div>
+  );
+}
