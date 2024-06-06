059dedabb9bc5992bda5dd88a7779a3394cd08e3
benny <vvsogi@gmail.com>
2024-06-06 16:51:01 +0900

    Add link to complete task1/choice

--- app/task1/choice/outer/page.tsx
+++ app/task1/choice/outer/page.tsx
@@ -2,6 +2,7 @@
 
 import React, { useEffect, useState } from "react";
 import { MoveBackHeader } from "@/app/components";
+import Link from "next/link";
 
 export default function Page() {
   const [data, setData] = useState<{ id: number; url: string }[]>([]);
@@ -21,7 +22,15 @@ export default function Page() {
 
   return (
     <div className="h-screen">
-      <MoveBackHeader path="/task1/choice" />
+      <div className="flex justify-between items-center">
+        <MoveBackHeader path="/task1/choice" />
+        <Link
+          className="w-[186px] h-[58.46px] mt-[48px] mr-[44px] text-[40px] text-center rounded-[30px] bg-[#2C2F37] text-white"
+          href="/task1/choice/result"
+        >
+          완료
+        </Link>
+      </div>
       <div className="mt-[79px] ml-[43px] mb-[76px]">
         <div className="w-[280px] h-[88px] flex justify-center items-center bg-[#2C2F37] rounded-[30px] text-[40px] text-white">
           하의
