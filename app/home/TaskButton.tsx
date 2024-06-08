ba50936486ffbee4e2b6be1140cab03d9243678b
benny <vvsogi@gmail.com>
2024-06-08 16:17:21 +0900

    Refactor TaskButton component and add task completion logic

--- app/home/TaskButton.tsx
+++ app/home/TaskButton.tsx
@@ -4,13 +4,16 @@ import React from "react";
 interface Props {
   path: string;
   children: React.ReactNode;
+  isComplete: boolean;
 }
 
-export function TaskButton({ children, path }: Props) {
+export function TaskButton({ children, path, isComplete }: Props) {
   return (
     <Link
-      className="w-[600px] h-[120px] flex justify-center items-center bg-[#2C2F37] text-white text-[64px] rounded-[50px]"
-      href={`/task${path}`}
+      className={`
+      ${isComplete ? "opacity-[30%]" : ""}
+      w-[600px] h-[120px] flex justify-center items-center bg-[#2C2F37] text-white text-[64px] rounded-[50px]`}
+      href={path}
     >
       {children}
     </Link>
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,74 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import { TaskButton } from "./home";
+import Link from "next/link";
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+  const [isCompleteTask1, setIsCompleteTask1] = useState(false);
+  const [isCompleteTask2, setIsCompleteTask2] = useState(false);
+  const [isCompleteTask3, setIsCompleteTask3] = useState(false);
+
+  useEffect(() => {
+    setLoading(false);
+    const task1 = (localStorage.getItem("task1") as string) || "{}";
+    const task2 = (localStorage.getItem("task2") as string) || "{}";
+    const task3 = (localStorage.getItem("task3") as string) || "{}";
+
+    const result1 = JSON.parse(task1);
+    const result2 = JSON.parse(task2);
+    const result3 = JSON.parse(task3);
+
+    if (result1.top || result1.bottom || result1.outer) {
+      setIsCompleteTask1(true);
+    }
+    if (result2.top || result2.bottom || result2.outer) {
+      setIsCompleteTask2(true);
+    }
+    if (result3.top || result3.bottom || result3.outer) {
+      setIsCompleteTask3(true);
+    }
+  }, []);
+
+  if (loading) return null;
+
   return (
     <main className="w-full h-screen">
       <div className="h-full flex flex-col justify-between py-[223px]">
         <div className="flex flex-col gap-[50px] mx-auto">
-          <TaskButton path="1">Task 1</TaskButton>
-          <TaskButton path="2">Task 2</TaskButton>
-          <TaskButton path="3">Task 3</TaskButton>
+          <TaskButton path="/task1" isComplete={isCompleteTask1}>
+            Task 1
+          </TaskButton>
+          <TaskButton path="/task2" isComplete={isCompleteTask2}>
+            Task 2
+          </TaskButton>
+          <TaskButton path="/task3" isComplete={isCompleteTask3}>
+            Task 3
+          </TaskButton>
+          <div
+            className={`
+              w-[600px] h-[120px] flex justify-center items-center bg-red-700 text-white text-[64px] rounded-[50px]
+            `}
+            onClick={() => {
+              localStorage.removeItem("task1");
+              localStorage.removeItem("task2");
+              localStorage.removeItem("task3");
+              setIsCompleteTask1(false);
+              setIsCompleteTask2(false);
+              setIsCompleteTask3(false);
+            }}
+          >
+            RESET
+          </div>
         </div>
         <div className="mx-auto">
-          <div className="w-[600px] h-[120px] flex justify-center items-center bg-[#808287] text-white text-[64px] rounded-[50px]">
+          <Link
+            className="w-[600px] h-[120px] flex justify-center items-center bg-[#808287] text-white text-[64px] rounded-[50px]"
+            href={"/result"}
+          >
             전체 결과 보기
-          </div>
+          </Link>
         </div>
       </div>
     </main>
