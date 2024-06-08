b59112c7534b988c9109a3bbe7faf3b5aa4089c6
benny <vvsogi@gmail.com>
2024-06-08 16:17:45 +0900

    Add result page component

---
+++ app/result/page.tsx
@@ -0,0 +1,23 @@
+import { MoveBackHeader } from "../components";
+import { TaskButton } from "../home";
+
+export default function Home() {
+  return (
+    <main className="w-full">
+      <MoveBackHeader path="/" />
+      <div className="h-full flex flex-col justify-between pt-[223px]">
+        <div className="flex flex-col gap-[50px] mx-auto">
+          <TaskButton path="/result/task1" isComplete={false}>
+            Task 1
+          </TaskButton>
+          <TaskButton path="/result/task2" isComplete={false}>
+            Task 2
+          </TaskButton>
+          <TaskButton path="/result/task3" isComplete={false}>
+            Task 3
+          </TaskButton>
+        </div>
+      </div>
+    </main>
+  );
+}
