d39e396691732c25d3a0349c1832274f3418674a
benny <vvsogi@gmail.com>
2024-06-08 15:57:25 +0900

    Add task3 choice components

---
+++ app/task3/choice/List.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+interface Props {
+  currentCategory: string;
+  targetCategory: string;
+  list: { id: number; url: string }[] | undefined;
+  choice: { id: number; url: string } | undefined;
+  onClick: (id: number) => void;
+}
+
+export default function List({
+  currentCategory,
+  targetCategory,
+  list,
+  choice,
+  onClick,
+}: Props) {
+  return (
+    currentCategory === targetCategory &&
+    list?.map((item) => {
+      return (
+        <div
+          key={item.id}
+          className={`
+          ${choice?.id === item.id ? "border-8 border-red-500" : ""}
+          w-full h-[300px]
+        `}
+          onClick={() => onClick(item.id)}
+        >
+          <img
+            className="w-full h-full object-cover"
+            src={item.url}
+            alt={item.url}
+          />
+        </div>
+      );
+    })
+  );
+}
---
+++ app/task3/choice/page.tsx
@@ -0,0 +1,159 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { MoveBackHeader } from "@/app/components";
+import Link from "next/link";
+import Category from "./Category";
+import List from "./List";
+
+export default function Page() {
+  const [category, setCategory] = useState<string>("top");
+
+  const [top, setTop] = useState<{ id: number; url: string }[]>();
+  const [bottom, setBottom] = useState<{ id: number; url: string }[]>();
+  const [outer, setOuter] = useState<{ id: number; url: string }[]>();
+
+  const [choiceTop, setChoiceTop] = useState<{ id: number; url: string }>();
+  const [choiceBottom, setChoiceBottom] = useState<{
+    id: number;
+    url: string;
+  }>();
+  const [choiceOuter, setChoiceOuter] = useState<{ id: number; url: string }>();
+  const [complete, setComplete] = useState<boolean>(false);
+
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem("task3") as string) || {};
+
+    fetch("http://localhost:3000/top.json").then(async (res) => {
+      const clothes = (await res.json()) as { id: number; url: string }[];
+      setChoiceTop(
+        data.top ? clothes.find((item) => item.id === data.top) : undefined
+      );
+      setTop(clothes);
+    });
+
+    fetch("http://localhost:3000/bottom.json").then(async (res) => {
+      const clothes = (await res.json()) as { id: number; url: string }[];
+      setChoiceBottom(
+        data.bottom
+          ? clothes.find((item) => item.id === data.bottom)
+          : undefined
+      );
+      setBottom(clothes);
+    });
+
+    fetch("http://localhost:3000/outer.json").then(async (res) => {
+      const clothes = (await res.json()) as { id: number; url: string }[];
+      setChoiceOuter(
+        data.outer ? clothes.find((item) => item.id === data.outer) : undefined
+      );
+      setOuter(clothes);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (choiceTop && choiceBottom && choiceOuter) {
+      setComplete(true);
+    }
+  }, [choiceTop, choiceBottom, choiceOuter]);
+
+  function choiceTopClothes(id: number) {
+    const findTop = top?.find((item) => item.id === id);
+    setChoiceTop(findTop);
+    const data = JSON.parse(localStorage.getItem("task3") as string) || {};
+    data.top = id;
+    localStorage.setItem("task3", JSON.stringify(data));
+  }
+
+  function choiceBottomClothes(id: number) {
+    const findBottom = bottom?.find((item) => item.id === id);
+    setChoiceBottom(findBottom);
+    const data = JSON.parse(localStorage.getItem("task3") as string) || {};
+    data.bottom = id;
+    localStorage.setItem("task3", JSON.stringify(data));
+  }
+
+  function choiceOuterClothes(id: number) {
+    const findOuter = outer?.find((item) => item.id === id);
+    setChoiceOuter(findOuter);
+    const data = JSON.parse(localStorage.getItem("task3") as string) || {};
+    data.outer = id;
+    localStorage.setItem("task3", JSON.stringify(data));
+  }
+
+  return (
+    <div>
+      <div className="flex justify-between items-center">
+        <MoveBackHeader path="/" />
+        {complete && (
+          <Link
+            className="w-[186px] h-[58.46px] mt-[48px] mr-[44px] text-[40px] text-center rounded-[30px] bg-[#2C2F37] text-white"
+            href="/task3/choice/result"
+          >
+            완료
+          </Link>
+        )}
+      </div>
+      <div className="flex gap-[25px] ml-[12px] mt-[24px]">
+        <Category
+          currentCategory={category}
+          targetCategory="top"
+          setCategory={setCategory}
+        >
+          상의
+        </Category>
+        <Category
+          currentCategory={category}
+          targetCategory="bottom"
+          setCategory={setCategory}
+        >
+          하의
+        </Category>
+        <Category
+          currentCategory={category}
+          targetCategory="outer"
+          setCategory={setCategory}
+        >
+          아우터
+        </Category>
+      </div>
+      <div className="w-full h-[600px] px-[12px] mt-[14.86px] grid grid-cols-4 overflow-scroll">
+        <List
+          currentCategory={category}
+          targetCategory="top"
+          list={top}
+          choice={choiceTop}
+          onClick={choiceTopClothes}
+        />
+        <List
+          currentCategory={category}
+          targetCategory="bottom"
+          list={bottom}
+          choice={choiceBottom}
+          onClick={choiceBottomClothes}
+        />
+        <List
+          currentCategory={category}
+          targetCategory="outer"
+          list={outer}
+          choice={choiceOuter}
+          onClick={choiceOuterClothes}
+        />
+      </div>
+      <div className="flex justify-center gap-[24px] mt-[91px]">
+        <img
+          className="w-[250px] h-[350px] object-cover border-black border-2"
+          src={choiceTop?.url}
+        />
+        <img
+          className="w-[250px] h-[350px] object-cover border-black border-2"
+          src={choiceBottom?.url}
+        />
+        <img
+          className="w-[250px] h-[350px] object-cover border-black border-2"
+          src={choiceOuter?.url}
+        />
+      </div>
+    </div>
+  );
+}
