9102adfaf2eaaedc98452a6e6ed366d9b6530f91
benny <vvsogi@gmail.com>
2024-06-10 18:42:47 +0900

    Update responsive styles for tablet and mobile

--- app/common/styles.ts
+++ app/common/styles.ts
@@ -1,7 +1,7 @@
 export const container = {
   size: {
     desktop: "2xl:block hidden",
-    tablet: "2xl:hidden lg:block hidden",
-    mobile: "lg:hidden",
+    tablet: "2xl:hidden md:block hidden",
+    mobile: "md:hidden",
   },
 };
