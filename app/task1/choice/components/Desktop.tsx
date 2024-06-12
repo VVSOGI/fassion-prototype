8ab628501c2661d27122ca61c88b2256183f95d9
benny <vvsogi@gmail.com>
2024-06-12 18:08:16 +0900

    Update Desktop and Mobile components

--- app/task1/choice/components/Desktop.tsx
+++ app/task1/choice/components/Desktop.tsx
@@ -1,21 +1,29 @@
 import React from "react";
 import {
-  MoveBackHeaderTablet,
-  MoveButtonTablet,
+  MoveBackHeaderDesktop,
+  MoveButtonDesktop,
   container,
 } from "@/app/common";
 
 export function Desktop() {
   return (
-    <div className={`${container.size.desktop}`}>
-      <MoveBackHeaderTablet path="/" />
-      <div className="flex flex-col gap-[50px] mt-[305px] mb-[175px]">
-        <MoveButtonTablet path="/task1/choice/top">상의</MoveButtonTablet>
-        <MoveButtonTablet path="/task1/choice/bottom">하의</MoveButtonTablet>
-        <MoveButtonTablet path="/task1/choice/outer">아우터</MoveButtonTablet>
-      </div>
-      <div className="flex mx-auto">
-        <MoveButtonTablet path="/task1/choice/result">코디맵</MoveButtonTablet>
+    <div className={`${container.size.desktop} h-screen`}>
+      <MoveBackHeaderDesktop path="/" />
+      <div className="h-[calc(100%-120px)] flex flex-col justify-center gap-[140px]">
+        <div className="flex flex-col gap-[50px]">
+          <MoveButtonDesktop path="/task1/choice/top">상의</MoveButtonDesktop>
+          <MoveButtonDesktop path="/task1/choice/bottom">
+            하의
+          </MoveButtonDesktop>
+          <MoveButtonDesktop path="/task1/choice/outer">
+            아우터
+          </MoveButtonDesktop>
+        </div>
+        <div className="flex mx-auto">
+          <MoveButtonDesktop path="/task1/choice/result">
+            코디맵
+          </MoveButtonDesktop>
+        </div>
       </div>
     </div>
   );
--- app/task1/choice/components/Mobile.tsx
+++ app/task1/choice/components/Mobile.tsx
@@ -1,21 +1,25 @@
 import React from "react";
 import {
-  MoveBackHeaderTablet,
+  MoveBackHeaderMobile,
   MoveButtonMobile,
   container,
 } from "@/app/common";
 
 export function Mobile() {
   return (
-    <div className={`${container.size.mobile}`}>
-      <MoveBackHeaderTablet path="/" />
-      <div className="flex flex-col gap-[50px] mt-[305px] mb-[175px]">
-        <MoveButtonMobile path="/task1/choice/top">상의</MoveButtonMobile>
-        <MoveButtonMobile path="/task1/choice/bottom">하의</MoveButtonMobile>
-        <MoveButtonMobile path="/task1/choice/outer">아우터</MoveButtonMobile>
-      </div>
-      <div className="flex mx-auto">
-        <MoveButtonMobile path="/task1/choice/result">코디맵</MoveButtonMobile>
+    <div className={`${container.size.mobile} h-screen`}>
+      <MoveBackHeaderMobile path="/" />
+      <div className="h-[calc(100%-74px)] flex flex-col justify-center gap-[72px]">
+        <div className="flex flex-col gap-[24px] px-[24px]">
+          <MoveButtonMobile path="/task1/choice/top">상의</MoveButtonMobile>
+          <MoveButtonMobile path="/task1/choice/bottom">하의</MoveButtonMobile>
+          <MoveButtonMobile path="/task1/choice/outer">아우터</MoveButtonMobile>
+        </div>
+        <div className="px-[24px]">
+          <MoveButtonMobile path="/task1/choice/result">
+            코디맵
+          </MoveButtonMobile>
+        </div>
       </div>
     </div>
   );
