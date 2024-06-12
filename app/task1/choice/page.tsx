51049c923101675d4383a95f2baf3cb76b1b2b99
benny <vvsogi@gmail.com>
2024-06-12 18:08:32 +0900

    Refactor page component to use separate components for different devices

--- app/task1/choice/page.tsx
+++ app/task1/choice/page.tsx
@@ -1,18 +1,12 @@
 import React from "react";
-import { MoveBackHeader, MoveButtonTablet } from "@/app/common";
+import { Desktop, Mobile, Tablet } from "./components";
 
 export default function page() {
   return (
     <div>
-      <MoveBackHeader path="/" />
-      <div className="flex flex-col gap-[50px] mt-[305px] mb-[175px]">
-        <MoveButtonTablet path="/task1/choice/top">상의</MoveButtonTablet>
-        <MoveButtonTablet path="/task1/choice/bottom">하의</MoveButtonTablet>
-        <MoveButtonTablet path="/task1/choice/outer">아우터</MoveButtonTablet>
-      </div>
-      <div className="flex mx-auto">
-        <MoveButtonTablet path="/task1/choice/result">코디맵</MoveButtonTablet>
-      </div>
+      <Desktop />
+      <Tablet />
+      <Mobile />
     </div>
   );
 }
