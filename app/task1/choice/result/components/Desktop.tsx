dc5bd18e9db6c22df0bbab2a30cd7d4a3678f55f
benny <vvsogi@gmail.com>
2024-06-20 18:15:14 +0900

    Add result page components for desktop and tablet

---
+++ app/task1/choice/result/components/Desktop.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { MoveBackHeaderDesktop, container } from "@/app/common";
+import Link from "next/link";
+import useResult from "@/app/hooks/useResult";
+
+export function Desktop() {
+  const { top, bottom, outer } = useResult("task1");
+
+  return (
+    <div className={container.size.desktop}>
+      <div className="flex justify-between items-center">
+        <MoveBackHeaderDesktop path="/task1/choice" />
+        <Link
+          className="w-[186px] h-[58.46px] mt-[48px] mr-[44px] text-[40px] text-center rounded-[30px] bg-[#2C2F37] text-white"
+          href="/task2"
+        >
+          Next
+        </Link>
+      </div>
+      <div className="absolute top-[140px] left-[43px] w-[144px] h-[40px] flex justify-center items-center bg-[#2C2F37] rounded-[30px] text-[20px] text-white">
+        코디맵
+      </div>
+      <div className="h-[calc(100vh-106.5px)] flex justify-center items-center gap-[38px]">
+        <div>
+          <div className="w-[250px] h-full bg-[#d9d9d9] border border-black">
+            {outer && (
+              <img className="w-full h-full" src={outer.url} alt={outer.url} />
+            )}
+          </div>
+        </div>
+        <div className="flex flex-col gap-[38px]">
+          <div className="w-[250px] h-full bg-[#d9d9d9] border border-black">
+            {top && (
+              <img className="w-full h-full" src={top.url} alt={top.url} />
+            )}
+          </div>
+          <div className="w-[250px] h-full bg-[#d9d9d9] border border-black">
+            {bottom && (
+              <img
+                className="w-full h-full"
+                src={bottom.url}
+                alt={bottom.url}
+              />
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
