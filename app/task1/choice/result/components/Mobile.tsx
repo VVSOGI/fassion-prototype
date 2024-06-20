91e0bbbec74ce10c21318acf0819118009cefd10
benny <vvsogi@gmail.com>
2024-06-20 18:24:57 +0900

    Add Mobile component to result page

--- app/task1/choice/result/components/Mobile.tsx
+++ app/task1/choice/result/components/Mobile.tsx
@@ -1,5 +1,57 @@
 import React from "react";
+import { container } from "@/app/common";
+import Link from "next/link";
+import useResult from "@/app/hooks/useResult";
+import { IoIosArrowBack } from "react-icons/io";
+import { useRouter } from "next/navigation";
 
 export function Mobile() {
-  return <div>Mobile</div>;
+  const { top, bottom, outer } = useResult("task1");
+  const router = useRouter();
+
+  return (
+    <div className={container.size.mobile}>
+      <div className="flex justify-between items-center pt-[24px] px-[24px]">
+        <IoIosArrowBack
+          className="cursor-pointer"
+          fontSize={50}
+          onClick={() => router.push("/task1/choice")}
+        />
+        <Link
+          className="w-fit px-[24px] py-[4px] bg-[#2C2F37] rounded-[30px] text-[18px] text-white"
+          href="/task2"
+        >
+          Next
+        </Link>
+      </div>
+      <div className="absolute top-[100px] left-[24px] w-fit h-[40px] px-[24px] flex justify-center items-center bg-[#2C2F37] rounded-[30px] text-[18px] text-white">
+        코디맵
+      </div>
+      <div className="h-[calc(100vh-74px)] flex justify-center items-center gap-[12px] px-[24px]">
+        <div>
+          <div className="w-full h-full bg-[#d9d9d9] border border-black">
+            {outer && (
+              <img className="w-full h-full" src={outer.url} alt={outer.url} />
+            )}
+          </div>
+        </div>
+        <div className="flex flex-col gap-[12px]">
+          <div className="w-full h-full bg-[#d9d9d9] border border-black">
+            {top && (
+              <img className="w-full h-full" src={top.url} alt={top.url} />
+            )}
+          </div>
+          <div className="w-full h-full bg-[#d9d9d9] border border-black">
+            {bottom && (
+              <img
+                className="w-full h-full"
+                src={bottom.url}
+                alt={bottom.url}
+              />
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- app/task1/choice/result/page.tsx
+++ app/task1/choice/result/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import React from "react";
-import { Desktop, Tablet } from "./components";
+import { Desktop, Mobile, Tablet } from "./components";
 
 export default function Page() {
   return (
     <div>
       <Desktop />
       <Tablet />
+      <Mobile />
     </div>
   );
 }
