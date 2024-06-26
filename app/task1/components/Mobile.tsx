34388ab76af7ffe9cf5fa33736dcd786b9269ef7
benny <vvsogi@gmail.com>
2024-06-26 17:26:51 +0900

    Update Mobile component styling

--- app/task1/components/Mobile.tsx
+++ app/task1/components/Mobile.tsx
@@ -5,10 +5,12 @@ export function Mobile() {
   return (
     <div className={`${container.size.mobile}`}>
       <div className="h-screen flex flex-col justify-center">
-        <div className="w-[400px] h-[80px] flex justify-center items-center mx-auto mb-[48px] border border-black rounded-[50px] text-[32px]">
-          Task 1
+        <div className="px-[24px]">
+          <div className="w-full h-[80px] flex justify-center items-center mb-[48px] border border-black rounded-[50px] text-[32px]">
+            Task 1
+          </div>
         </div>
-        <div className="h-[152px] text-[32px] text-center">
+        <div className="h-[152px] text-[24px] text-center">
           출근할 때 입고 싶은 옷의 조합을 <br /> 코디해 주세요
         </div>
         <div className="px-[24px]">
