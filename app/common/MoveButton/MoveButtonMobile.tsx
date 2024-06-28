4c5750914814cc1f503890783726a6b0fc720a9e
benny <vvsogi@gmail.com>
2024-06-28 17:43:35 +0900

    Update button styling in MoveButtonMobile and Mobile components

--- app/common/MoveButton/MoveButtonMobile.tsx
+++ app/common/MoveButton/MoveButtonMobile.tsx
@@ -9,7 +9,7 @@ interface Props {
 export function MoveButtonMobile({ path, children }: Props) {
   return (
     <Link
-      className="w-full h-[80px] flex justify-center items-center bg-[#2C2F37] rounded-[50px] text-[32px] text-white mx-auto"
+      className="w-full flex justify-center items-center py-[4px] bg-[#2C2F37] rounded-[50px] text-[24px] text-white mx-auto"
       href={path}
     >
       {children}
--- app/task1/components/Mobile.tsx
+++ app/task1/components/Mobile.tsx
@@ -6,11 +6,11 @@ export function Mobile() {
     <div className={`${container.size.mobile}`}>
       <div className="h-screen flex flex-col justify-center">
         <div className="px-[24px]">
-          <div className="w-full h-[80px] flex justify-center items-center mb-[48px] border border-black rounded-[50px] text-[32px]">
+          <div className="w-full flex justify-center items-center py-[4px] mb-[48px] border border-black rounded-[50px] text-[24px]">
             Task 1
           </div>
         </div>
-        <div className="h-[152px] text-[24px] text-center">
+        <div className="mb-[48px] text-[24px] text-center">
           출근할 때 입고 싶은 옷의 조합을 <br /> 코디해 주세요
         </div>
         <div className="px-[24px]">
